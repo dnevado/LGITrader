ac20cf40b9a02e8e238f9eaa568bcc3c2675643a
David Nevado <david_nevado@yahoo.es>
2017-12-30 19:25:05 +0100

    iteration improved, crons improved

--- modules/LGITrader/src/main/java/com/trader/dashboard/portlet/portlet/GITraderPortlet.java
+++ modules/LGITrader/src/main/java/com/trader/dashboard/portlet/portlet/GITraderPortlet.java
@@ -13,9 +13,12 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.OrganizationConstants;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 
 import java.util.List;
@@ -53,129 +56,84 @@ public class GITraderPortlet extends MVCPortlet {
 		// TODO Auto-generated method stub
 		super.init();
 		/* fill up with required data */
-		
-		long guestGroupId=0;
 		Config  _conf = null;
-		_log.info("Verifying Configuration Table for All Companies" );
+		_log.info("Verifying Configuration Table for All Organizations/Company" );
 		List<Company> lCompanies = CompanyLocalServiceUtil.getCompanies();
-		if (!lCompanies.isEmpty())
+		long guestGroupId =0;
+		/* VALORES GENERALES DE LA COMPAÑIA */
+		for (Company _Company : lCompanies )
 		{
-			for (Company _Company : lCompanies )
+			try {
+				guestGroupId = GroupLocalServiceUtil.getGroup(_Company.getCompanyId(), GroupConstants.GUEST).getGroupId();
+			} catch (PortalException e) {
+			// TODO Auto-generated catch block
+				e.printStackTrace();			
+			}
+			_conf = ConfigLocalServiceUtil.findByKeyCompanyGroup(IBTraderConstants.keyCRON_TRADING_STATUS, _Company.getCompanyId(), guestGroupId);
+			int _TotalConfigs = (_conf!=null ? 1 : 0 );
+			if (_TotalConfigs==0)
 			{
-				
-				try {
-					guestGroupId = GroupLocalServiceUtil.getGroup(_Company.getCompanyId(), GroupConstants.GUEST).getGroupId();
-				} catch (PortalException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				int _TotalConfigs = ConfigLocalServiceUtil.getConfigsCount();
-				if (_TotalConfigs==0)
-				{
-					  /* TWS HOST */
-					 _log.info("Adding missing Config Table Values .." );
-					 
-					  //_conf = new ConfigImpl();
-					 
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setConfig_key(IBTraderConstants.keyTWS_HOST);
-					 _conf.setValue(IBTraderConstants.vTWS_HOST);
-					 _conf.setName(IBTraderConstants.keyTWS_HOST);
-					 _conf.setDescription(IBTraderConstants.keyTWS_HOST);
-					 _conf.setGlobaldefault(true);					 
-					 ConfigLocalServiceUtil.updateConfig(_conf);
-					  
-					 /* TWS ACOOUNT */
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setConfig_key(IBTraderConstants.keyACCOUNT_IB_NAME);
-					 _conf.setValue(IBTraderConstants.vACCOUNT_IB_NAME);
-					 _conf.setName(IBTraderConstants.keyACCOUNT_IB_NAME);
-					 _conf.setDescription(IBTraderConstants.keyACCOUNT_IB_NAME);
-					 _conf.setGlobaldefault(true);
-					 ConfigLocalServiceUtil.updateConfig(_conf);
-					 
-					 /* TWS PORT  */
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setConfig_key(IBTraderConstants.keyTWS_PORT);
-					 _conf.setValue(String.valueOf(IBTraderConstants.vTWS_PORT));
-					 _conf.setName(IBTraderConstants.keyTWS_PORT);
-					 _conf.setDescription(IBTraderConstants.keyTWS_PORT);
-					 _conf.setGlobaldefault(true);
-					 ConfigLocalServiceUtil.updateConfig(_conf);
-					 
-					 /* TWS tick future  */
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setConfig_key(IBTraderConstants.keyDEFAULT_TICK_FUTURE);
-					 _conf.setValue(String.valueOf(IBTraderConstants.vDEFAULT_TICK_FUTURE));
-					 _conf.setName(IBTraderConstants.keyDEFAULT_TICK_FUTURE);
-					 _conf.setDescription(IBTraderConstants.keyDEFAULT_TICK_FUTURE);
-					 ConfigLocalServiceUtil.updateConfig(_conf);
-					 
-					 /* DIAS PARA MANTENER EL BACKUP   */
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setConfig_key(IBTraderConstants.keyNUM_DAYS_PAST_REALTIME);
-					 _conf.setValue(String.valueOf(IBTraderConstants.vNUM_DAYS_PAST_REALTIME));
-					 _conf.setName(IBTraderConstants.keyNUM_DAYS_PAST_REALTIME);
-					 _conf.setDescription(IBTraderConstants.keyNUM_DAYS_PAST_REALTIME);
-					 _conf.setGlobaldefault(true);
-					 ConfigLocalServiceUtil.updateConfig(_conf);
-					 
-					 
-					 /* PREVENT CRON TRADE  CONCURRENCY */   
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setConfig_key(IBTraderConstants.keyCRON_TRADING_STATUS);
-					 _conf.setValue(String.valueOf(IBTraderConstants.vCRON_TRADING_STATUS));
-					 _conf.setName(IBTraderConstants.keyCRON_TRADING_STATUS);
-					 _conf.setDescription(IBTraderConstants.keyCRON_TRADING_STATUS);
-					 _conf.setGlobaldefault(true);
-					 ConfigLocalServiceUtil.updateConfig(_conf); 
-					 
-					 /* PREVENT CRON READ CONCURRENCY */   
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setConfig_key(IBTraderConstants.keyCRON_READING_STATUS);
-					 _conf.setValue(String.valueOf(IBTraderConstants.vCRON_READING_STATUS));
-					 _conf.setName(IBTraderConstants.keyCRON_READING_STATUS);
-					 _conf.setDescription(IBTraderConstants.keyCRON_READING_STATUS);
-					 _conf.setGlobaldefault(true);
-					 ConfigLocalServiceUtil.updateConfig(_conf);   
-					 
-					 
-					 /* SIMULATION MODE */   
-					 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
-					 _conf.setGroupId(guestGroupId);
-					 _conf.setCompanyId(_Company.getCompanyId());
-					 _conf.setConfig_key(IBTraderConstants.keySIMULATION_MODE);
-					 _conf.setValue(String.valueOf(IBTraderConstants.vSIMULATION_MODE));
-					 _conf.setName(IBTraderConstants.keySIMULATION_MODE);
-					 _conf.setDescription(IBTraderConstants.keySIMULATION_MODE);
-					 _conf.setGlobaldefault(true);
-					 ConfigLocalServiceUtil.updateConfig(_conf);   
-					 
-					 
-				}
-				
+				/* TWS tick future  */
+				 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
+				 _conf.setCompanyId(_Company.getCompanyId());
+				 _conf.setGroupId(guestGroupId);
+				 _conf.setConfig_key(IBTraderConstants.keyDEFAULT_TICK_FUTURE);
+				 _conf.setValue(String.valueOf(IBTraderConstants.vDEFAULT_TICK_FUTURE));
+				 _conf.setName(IBTraderConstants.keyDEFAULT_TICK_FUTURE);
+				 _conf.setDescription(IBTraderConstants.keyDEFAULT_TICK_FUTURE);
+				 ConfigLocalServiceUtil.updateConfig(_conf);
+				 
+				 /* DIAS PARA MANTENER EL BACKUP   */
+				 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
+				 _conf.setCompanyId(_Company.getCompanyId());
+				 _conf.setGroupId(guestGroupId);
+				 _conf.setConfig_key(IBTraderConstants.keyNUM_DAYS_PAST_REALTIME);
+				 _conf.setValue(String.valueOf(IBTraderConstants.vNUM_DAYS_PAST_REALTIME));
+				 _conf.setName(IBTraderConstants.keyNUM_DAYS_PAST_REALTIME);
+				 _conf.setDescription(IBTraderConstants.keyNUM_DAYS_PAST_REALTIME);
+				 _conf.setGlobaldefault(false);
+				 ConfigLocalServiceUtil.updateConfig(_conf);
+				 
+				 
+				 /* PREVENT CRON TRADE  CONCURRENCY */   
+				 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
+				 _conf.setCompanyId(_Company.getCompanyId());
+				 _conf.setGroupId(guestGroupId);
+				 _conf.setConfig_key(IBTraderConstants.keyCRON_TRADING_STATUS);
+				 _conf.setValue(String.valueOf(IBTraderConstants.vCRON_TRADING_STATUS));
+				 _conf.setName(IBTraderConstants.keyCRON_TRADING_STATUS);
+				 _conf.setDescription(IBTraderConstants.keyCRON_TRADING_STATUS);
+				 _conf.setGlobaldefault(false);
+				 ConfigLocalServiceUtil.updateConfig(_conf); 
+				 
+				 /* PREVENT CRON READ CONCURRENCY */   
+				 _conf = ConfigLocalServiceUtil.createConfig(CounterLocalServiceUtil.increment(Config.class.getName()));
+				 _conf.setCompanyId(_Company.getCompanyId());
+				 _conf.setGroupId(guestGroupId);
+				 _conf.setConfig_key(IBTraderConstants.keyCRON_READING_STATUS);
+				 _conf.setValue(String.valueOf(IBTraderConstants.vCRON_READING_STATUS));
+				 _conf.setName(IBTraderConstants.keyCRON_READING_STATUS);
+				 _conf.setDescription(IBTraderConstants.keyCRON_READING_STATUS);
+				 _conf.setGlobaldefault(false);
+				 ConfigLocalServiceUtil.updateConfig(_conf);   
 				
 			}
-		}
-		
-		
-			
-		
-		
+			List<Organization> lOrganization = OrganizationLocalServiceUtil.getOrganizations(_Company.getCompanyId(), OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID, 0, OrganizationLocalServiceUtil.getOrganizationsCount()+1);
+			for (Organization _Organization : lOrganization )
+			{
+				/* BUSCAMOS UNA CLAVE PARA VER SI EXISTE PARA LA ORGANIZACION */
+				ConfigLocalServiceUtil.addConfigurationValuesCompanyGroup(_Organization.getCompanyId(), _Organization.getGroupId());
+				//addConfigurationValuesCompanyGroup
+			}			
+				
+			_conf = ConfigLocalServiceUtil.findByKeyCompanyGroup(IBTraderConstants.keyCRON_READING_STATUS, _Company.getCompanyId(), guestGroupId);
+			_conf.setValue(String.valueOf(IBTraderConstants.vCRON_READING_STATUS));
+			ConfigLocalServiceUtil.updateConfig(_conf);
+			_conf = ConfigLocalServiceUtil.findByKeyCompanyGroup(IBTraderConstants.keyCRON_TRADING_STATUS , _Company.getCompanyId(), guestGroupId);
+			_conf.setValue(String.valueOf(IBTraderConstants.keyCRON_TRADING_STATUS));
+			ConfigLocalServiceUtil.updateConfig(_conf);					 
+		}//  end companies list 
+		/* VALORES PARA CADA EMPRESA */
 	}
 	
 	public  void  CronTrade(ActionRequest actionRequest, ActionResponse actionResponse)
@@ -194,6 +152,16 @@ public class GITraderPortlet extends MVCPortlet {
 		// TODO Auto-generated method stub
 		try {
 		//	CronUtil.StartReadingCron(new Message());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public  void  CronVerifyContract(ActionRequest actionRequest, ActionResponse actionResponse)
+			throws PortletException {
+		// TODO Auto-generated method stub
+		try {
+			CronUtil.StartVerifyContractsCron(new Message());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- modules/LGITrader/src/main/resources/META-INF/resources/view.jsp
+++ modules/LGITrader/src/main/resources/META-INF/resources/view.jsp
@@ -9,42 +9,8 @@
 
 <aui:a href="<%= CronTrade %>" >CronTrade</aui:a>
 
+<portlet:actionURL name="CronVerifyContract" var="CronVerifyContract" />
+
+<aui:a href="<%= CronVerifyContract %>" >CronVerifyContract</aui:a>
 
 
-
-<table id="dashboard" class="table responsive table-bordered table-hover table-striped"  style="width: 100%;"  role="grid">
-<thead>
-    <tr>    	
-        <th>Código</th>
-        <th>Titulo</th>               
-        <th>Descripción</th>
-        <th title=" Activo">Activo</th>
-        <th title=" Visible">Visible</th>
-        <th> </th>              
-    </tr>
-</thead>
-<tbody>
-<tr  class="ui-state-default">
-		<td>Código</td>
-        <td>Titulo</td>               
-        <td>Descripción</td>
-        <td title=" Activo">Activo</td>
-        <td title=" Visible">Visible</td>
-        <td> </th>     
-</tr>        
-</tbody>
-</table>
-
-
-<script>
-$( document ).ready(function() {			
-	 
-	 _DoctorTableList=$("#dashboard").DataTable( {
-		    responsive: true
-	 } );
-}); 
-
-</script>
-<p>
-	<b><liferay-ui:message key="LGITrader.caption"/></b>
-</p>
